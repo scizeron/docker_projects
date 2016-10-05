21ce6f025f0e58a3bda15621b9e3fb688834a45b
scizeron <stephane.cizeron@gmail.com>
2016-10-05 12:38:05 +0000

    redis loop

--- redis-cli/entrypoint.sh
+++ redis-cli/entrypoint.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-redis-cli -h $1 -p $2 -a $3 info | grep keys | grep db0 | cut -d "," -f  1 | cut -d "=" -f 2
+if [ $# -lt 3 ]
+then
+ echo "missing parameters"
+ exit 1
+fi
+
+while [ 1 ]
+do
+ redis-cli -h $1 -p $2 -a $3 info | grep keys | grep db0 | cut -d "," -f  1 | cut -d "=" -f 2
+ sleep 5
+done
+
+exit 0
